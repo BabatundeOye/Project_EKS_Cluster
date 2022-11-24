a48f5ea7f50e19ec69a65ab13f59ae570f78a750
Babatunde Oye <teamsoldd@gmail.com>
2022-11-24 04:09:22 +0000

    eks_cluster

--- providers.tf
+++ providers.tf
@@ -14,6 +14,8 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "http" {}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@ variable "aws_region" {
   type    = string
   default = "us-west-2"
 }
+
+variable "access_key" {}
+variable "secret_key" {}
